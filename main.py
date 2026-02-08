d81a023143a570d04cc37b5f4c9e07e616fbea49
Mstafa-samer <aboodbashar9@gmail.com>
2026-02-08 23:01:06 +0300

    .
    /

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.requests import Request
+from fastapi.staticfiles import StaticFiles
+from router.user import router as user_router
+app = FastAPI()
+# 1️⃣ ربط مجلد static للـ CSS
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+
+# 3️⃣ إضافة router مع تمرير templates
+app.include_router(user_router, prefix="/user", tags=["user"],
+                   responses={404: {"description": "Not found"}},
+                   # هنا نمرر templates كـ dependency
+)
